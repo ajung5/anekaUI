304a8d5a422375fbbdb7a4ee11bab8f914089623
Agung Nawawi <agung@fairtech.com.sg>
2024-02-11 18:00:43 +0700

    Initial Commit

---
+++ anekaUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  anekaUI
+//
+//  Created by Agung Nawawi on 11/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ anekaUI/anekaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  anekaUIApp.swift
+//  anekaUI
+//
+//  Created by Agung Nawawi on 11/02/24.
+//
+
+import SwiftUI
+
+@main
+struct anekaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
